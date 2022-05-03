be56d337e4031ccabe6a3bed7caa40a7cdb0bf27
haroldsenparker <103767345+haroldsenparker@users.noreply.github.com>
2022-05-03 10:18:07 -0600

    Add files via upload

---
+++ MadLib Group Project/MadLib Group Project/View Controllers/JessicaViewController2.swift
@@ -0,0 +1,29 @@
+//
+//  JessicaViewController2.swift
+//  MadLib Group Project
+//
+//  Created by Parker Haroldsen on 5/3/22.
+//
+
+import UIKit
+
+class JessicaViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
