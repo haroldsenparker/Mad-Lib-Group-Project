bd2a8abdb54f8be0868d94702f60ca74198abcac
Parker Haroldsen <haroldsen.p15@gmail.com>
2022-05-11 09:24:44 -0600

    redo view controllers

--- MadLib Group Project/MadLib Group Project/View Controllers/80sMovieViewController.swift
+++ MadLib Group Project/MadLib Group Project/View Controllers/80sMovieViewController.swift
@@ -18,16 +18,9 @@ class _0sMovieViewController: UIViewController {
     @IBOutlet var adverb1: UITextField!
     @IBOutlet var place: UITextField!
     @IBOutlet var noun2: UITextField!
-    
-    @IBOutlet var lblEnter: UILabel!
+
     
     @IBAction func btnEnter(_ sender: Any) {
-        let stories: NSArray = ["One day, Jack Burton came across a giant, mutated animal on the top of his truck during the time of day. The monster was adjective1 and was verb1 around wildly. Jack destroyed this creature with a noun1 and slowly slumped to the ground. From where it came, he knew not, but he had a strong inclination it was from the place. He proclaimed aloud: Just remember what ol’ Jack Burton does when the earth quakes, and the poison arrows fall from the sky, and the pillars of Heaven shake. Yeah, Jack Burton just looks that big ol’ noun2  right square in the eye and he says, “Give me your best shot, pal. I can take it.” ",  "Jack Slater is an everyday normal adjective1 man. One time of day, a noun1 explodes nearby, he soon realizes that he is being chased by the government who’s trying to verb1 him.  While on the run, he teams up with an incredibly attractive animal with an incredible power to turn into noun2. The duo decides to turn the tables by blowing up place1 that causes the local police to verb2. "
-           , "Gizmo the Mogwai was just going about his day in the “time of day”, when suddenly an “animal” appeared across his path. It was unlike anything he has seen before! It has somehow been corrupted by his archenemy, Stripe. According to its now “adjective” eyes and a stripe running down the front of its head like a tattoo, it seems like that is the case. It suddenly “verb1” towards Gizmo and “verb2” him in the face. Since, Stripe attacked the Clamp Center, he trained, and gained proficiency in “noun1”. So, he put this sad, little creature out to the pasture. "]
-        let numbers: UInt32 = UInt32(stories.count)
-        let randomNumber = Int(arc4random_uniform(numbers))
-            let storyString = stories.object(at: randomNumber)
-            self.lblEnter.text = storyString as? String
-    }
+       
     
 }
--- MadLib Group Project/MadLib Group Project/View Controllers/GoosebumpsViewController.swift
+++ MadLib Group Project/MadLib Group Project/View Controllers/GoosebumpsViewController.swift
@@ -16,14 +16,8 @@ class GoosebumpsViewController: UIViewController {
     @IBOutlet var adj2: UITextField!
     @IBOutlet var adj3: UITextField!
     
-    @IBOutlet var lblEnter: UILabel!
-    
     @IBAction func btnEnter(_ sender: Any) {
-        let stories: NSArray = ["Name1 and Name2 think the old place1 they have just moved into is weird. Spooky. Possibly adjective1. And the town of Dark Falls is pretty adjective2, too. But their parents don't believe them. You'll get used to it, they say. Go out and make some adjective3 friends. So Name1 and Name2 do. But these new friends are not exactly what their parents had in mind. Because they want to be friends......forever.", "Name1 thinks there is something wrong with the old camera he and his friends found. The photographs keep turning out wrong. Very wrong. Like the snapshot Name1 took of his father's new car that shows it totaled. And then Name1’'s father is in a nasty wreck. But Name1’'s friends don't believe him. Name2 even makes Name1 bring the camera to her birthday party and take her picture. Only Name2 ’s not in the photograph when it develops. Is Name2 about to be taken out of the picture permanently? Who is going to take the next fall for... the evil camera?", "When Name1 finds a dusty old piano in the place of his new house, his parents offer to pay for lessons. At first, taking piano seems like a cool idea. But there's something creepy about Name1's piano teacher, Dr. Name2. Something creepy. Something Name1 can't quite put his finger on. Then Name1 hears the stories. Terrifying stories. About the students at Dr. Name2's music school. Students who went in for a lesson... and never came out."]
-        let numbers: UInt32 = UInt32(stories.count)
-        let randomNumber = Int(arc4random_uniform(numbers))
-            let storyString = stories.object(at: randomNumber)
-            self.lblEnter.text = storyString as? String
+    
     }
     
 }
