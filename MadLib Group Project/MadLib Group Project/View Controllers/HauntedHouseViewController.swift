8e26ff04bcee4cfb6fa81bf8ba1b6ea880b6e727
Jessica Gonzalez <jgonzalez34559@gmail.com>
2022-05-13 08:19:43 -0600

    Added another viewcontroller and connections
    Added another view controller for the haunted house story and added connections for all 3 stories

--- MadLib Group Project/MadLib Group Project/View Controllers/HauntedHouseViewController.swift
+++ MadLib Group Project/MadLib Group Project/View Controllers/HauntedHouseViewController.swift
@@ -1,31 +1,28 @@
 //
-//  JessicaViewController1.swift
+//  HauntedHouseViewController.swift
 //  MadLib Group Project
 //
-//  Created by Parker Haroldsen on 5/3/22.
+//  Created by Jessica Gonzalez on 5/13/22.
 //
 
 import UIKit
 
-class Dream: UIViewController {
+class HauntedHouseViewController: UIViewController {
 
     @IBOutlet weak var adjetive1: UITextField!
-    @IBOutlet weak var noun1: UITextField!
     @IBOutlet weak var name1: UITextField!
-    @IBOutlet weak var animal1: UITextField!
-    @IBOutlet weak var verb1: UITextField!
-    @IBOutlet weak var name2: UITextField!
-    @IBOutlet weak var noun2: UITextField!
-    @IBOutlet weak var noun3: UITextField!
     @IBOutlet weak var adjetive2: UITextField!
-    
-    
+    @IBOutlet weak var noun1: UITextField!
+    @IBOutlet weak var verb1: UITextField!
+    @IBOutlet weak var adjetive3: UITextField!
+    @IBOutlet weak var bodyPart1: UITextField!
+    @IBOutlet weak var bodyPart2: UITextField!
+    @IBOutlet weak var name2: UITextField!
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
     
 
--- MadLib Group Project/MadLib Group Project/View Controllers/HerosQuest.swift
+++ MadLib Group Project/MadLib Group Project/View Controllers/HerosQuest.swift
@@ -9,10 +9,23 @@ import UIKit
 
 class HerosQuest: UIViewController {
 
+    @IBOutlet weak var name1: UITextField!
+    @IBOutlet weak var noun1: UITextField!
+    @IBOutlet weak var noun2: UITextField!
+    @IBOutlet weak var adjetive1: UITextField!
+    @IBOutlet weak var adjetive2: UITextField!
+    @IBOutlet weak var verb1: UITextField!
+    @IBOutlet weak var noun3: UITextField!
+    @IBOutlet weak var adjetive3: UITextField!
+    @IBOutlet weak var name2: UITextField!
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
