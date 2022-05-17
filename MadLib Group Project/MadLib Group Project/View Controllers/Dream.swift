9f3cb854ce0bcbc029efbdb572b25d76f39fde3c
Jessica Gonzalez <jgonzalez34559@gmail.com>
2022-05-17 10:26:03 -0600

    More code and connections
    Story codes and connections for 3 view controllers

--- MadLib Group Project/MadLib Group Project/View Controllers/Dream.swift
+++ MadLib Group Project/MadLib Group Project/View Controllers/Dream.swift
@@ -19,12 +19,9 @@ class Dream: UIViewController {
     @IBOutlet weak var noun3: UITextField!
     @IBOutlet weak var adjetive2: UITextField!
     
+    @IBOutlet weak var labelEnter: UILabel!
     
-    
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    @IBAction func enterButtonTapped(_ sender: UIButton) {
+        labelEnter.text = "Last night, I had a " + adjetive1.text! + " dream. There were " + noun1.text! + " flying everywhere! My best friend, " + name1.text! + ", had turned into a talking " + animal1.text! + ". Everywhere we went, there was always someone " + verb1.text! + " behind me. It all felt so real until I looked into the mirror. It wasn’t me at all! Instead, it was " + name2.text! + "! After that, I immediately woke up. “Phew, glad that is over.” Or at least I thought! My mom had turned into a " + noun2.text! + "! I reached to the side of my bed for my trusty " + noun3.text! + " thinking it would save my mom. Just as I was about to use it, my mom woke me up for breakfast. “Are you okay? You look " + adjetive2.text! + ".” I hugged her and replied “Yeah, just peachy.”"
     }
 }
--- MadLib Group Project/MadLib Group Project/View Controllers/HerosQuest.swift
+++ MadLib Group Project/MadLib Group Project/View Controllers/HerosQuest.swift
@@ -20,11 +20,9 @@ class HerosQuest: UIViewController {
     @IBOutlet weak var name2: UITextField!
     
     
+    @IBOutlet weak var labelEnter: UILabel!
     
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    @IBAction func enterButtonTapped(_ sender: UIButton) {
+        labelEnter.text = "“Hero! You’re finally here! We must stop " + name1.text! + " from taking over the land of " + noun1.text! + "!“ You and your trusty " + noun2.text! + "head out deep in the woods where your arch nemesis resides. “Watch out hero! Here comes a monster!” The monster looks rather " + adjetive1.text! + ". “Quick! Use your " + adjetive2.text! + " sword to slay the monster!” You defeat the monster with ease and move forward. After defeating many monsters, you finally reach your destination. “Look Hero, there they are! Now is the perfect time to attack!” You reach for your sword and " + verb1.text! + " towards the enemy. You scream with all your might, “SWORD OF " + noun3.text! + ", LEND ME YOUR STRENGTH!” The sword glows and next thing you know, your foe has been defeated. “Wow, that was " + adjetive3.text! + " hero!” You pause for a moment and reply with “Actually, my name is " + name2.text! + "."
     }
 }
