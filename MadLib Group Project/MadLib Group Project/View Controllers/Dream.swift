1fb81ce79fa05c90b909002a5254a8ed99285827
Jessica Gonzalez <jgonzalez34559@gmail.com>
2022-06-21 10:38:00 -0600

    Updated Dream and Haunted View Controllers
    Updated view controllers for both Dream and Haunted.

--- MadLib Group Project/MadLib Group Project/View Controllers/Dream.swift
+++ MadLib Group Project/MadLib Group Project/View Controllers/Dream.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class Dream: UIViewController {
-
+    
     @IBOutlet weak var adjetive1: UITextField!
     @IBOutlet weak var noun1: UITextField!
     @IBOutlet weak var name1: UITextField!
@@ -19,9 +19,32 @@ class Dream: UIViewController {
     @IBOutlet weak var noun3: UITextField!
     @IBOutlet weak var adjetive2: UITextField!
     
-    @IBOutlet weak var labelEnter: UILabel!
     
     @IBAction func enterButtonTapped(_ sender: UIButton) {
-        labelEnter.text = "Last night, I had a " + adjetive1.text! + " dream. There were " + noun1.text! + " flying everywhere! My best friend, " + name1.text! + ", had turned into a talking " + animal1.text! + ". Everywhere we went, there was always someone " + verb1.text! + " behind me. It all felt so real until I looked into the mirror. It wasn’t me at all! Instead, it was " + name2.text! + "! After that, I immediately woke up. “Phew, glad that is over.” Or at least I thought! My mom had turned into a " + noun2.text! + "! I reached to the side of my bed for my trusty " + noun3.text! + " thinking it would save my mom. Just as I was about to use it, my mom woke me up for breakfast. “Are you okay? You look " + adjetive2.text! + ".” I hugged her and replied “Yeah, just peachy.”"
+        guard !adjetive1.text!.isEmpty else {
+            print("Please fill in all text fields.")
+            return
+        }
+        performSegue(withIdentifier: "displayStory", sender: nil)
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! DisplayStoryViewController
+        destinationVC.madlib = currentMadLib()
+    }
+    
+    func currentMadLib() -> Madlib? {
+        let dream = [story1()!, story2()!]
+        return Madlib(backgroundColor: .systemPink, stories: dream)
+    }
+    
+    func story1() -> String? {
+        return "Last night, I had a \(adjetive1.text!) dream. There were \(noun1.text!) flying everywhere! My best friend, \(name1.text!), had turned into a talking \(animal1.text!)! Everywhere we went, there was always someone \(verb1.text!) behind me. It all felt so real until I looked into the mirror. It wasn’t me at all! Instead, it was \(name2.text!)! After that, I immediately woke up. 'Phew, glad that is over.' Or at least I thought! My mom had turned into a \(noun2.text!)! I reached to the side of my bed for my trusty \(noun3.text!) thinking it would save my mom. Just as I was about to use it, my mom woke me up for breakfast. 'Are you okay? You look \(adjetive2.text!).' I hugged her and said 'Yeah, just peachy.'"
+    }
+    
+    func story2() -> String? {
+        return "\(animal1.text!) live in our walls but do not trouble our kitchen. We are \(adjetive1.text!) but cannot understand why they do not come into our kitchen where we have traps set, as they come into the kitchens of \(name1.text!). Although we are pleased, we are also upset, because the \(noun1.text!) behave as though there were something wrong with our kitchen. What makes this even more puzzling is that our house is much less tidy than the houses of our neighbors. There is more food \(verb1.text!) about in our kitchen, more crumbs on the counters and filthy scraps of \(noun2.text!) kicked against the base of the cabinets. In fact, there is so much loose food in the kitchen I can only think \(name2.text!) themselves are defeated by it. In a \(adjetive2.text!) kitchen, it is a challenge for them to find enough food night after night to survive until spring. They patiently hunt and nibble hour after hour until they are satisfied. In the \(noun3.text!), however, they are faced with something so out of proportion to their experience that they cannot deal with it. They might venture out a few steps, but soon the overwhelming sights and smells drive them back into their holes, uncomfortable and embarrassed at not being able to scavenge as they should."
+
+    }
+    
 }
--- MadLib Group Project/MadLib Group Project/View Controllers/HauntedHouseViewController.swift
+++ MadLib Group Project/MadLib Group Project/View Controllers/HauntedHouseViewController.swift
@@ -19,9 +19,28 @@ class HauntedHouseViewController: UIViewController {
     @IBOutlet weak var bodyPart2: UITextField!
     @IBOutlet weak var name2: UITextField!
     
-    @IBOutlet weak var labelEnter: UILabel!
     
     @IBAction func enterButtonTapped(_ sender: UIButton) {
-        labelEnter.text = "It was a dark and " + adjective1.text! + " night. The house across the street had been abandoned for as long as I can remember. I called up " + name1.text! + " and asked if they wanted to help me sneak into the house. “Why do you want to go inside that house?” “Well, because I am " + adjetive2.text! + ".” “Well I’m not. Goodnight. They hung up. Oh well, guess I will go on my own. I grabbed my trusty " + noun1.text! + "and was on my way. The door to the house was locked so I decided to " + verb1.text! + " the door. “Wow, it actually worked. " +  adjetive3.text! + "!” I entered the house and the door immediately shut behind me. It was too dark and I couldn’t move. I felt a cold " + bodyPart1.text! + " touch my " + bodyPart2.text! + ". I ran out the door and back into my home. I looked out the window into the house to see " + name2.text! + " looking back at me."
+        guard !adjective1.text!.isEmpty else {
+            print("Please fill in all text fields.")
+            return
+        }
+        performSegue(withIdentifier: "displayStory", sender: nil)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! DisplayStoryViewController
+        destinationVC.madlib = currentMadLib()
+    }
+    func currentMadLib() -> Madlib? {
+        let spooky = [story1()!, story2()!]
+        return Madlib(backgroundColor: .purple, stories: spooky)
+    }
+    
+    func story1() -> String? {
+        return "It was a dark and \(adjective1.text!) night. The house across the street had been abandoned for as long as I can remember. I called up \(name1.text!) and asked if they wanted to help me sneak into the house. 'Why do you want to go inside that house?' ' Well, because I am \(adjetive2.text!)' 'Well I’m not. Goodnight.' They hung up. Oh well, guess I will go on my own. I grabbed my trusty \(noun1.text!) and was on my way. The door to the house was locked so I decided to \(verb1.text!) the door. 'Wow, it actually worked. \(adjetive3.text!)!' I entered the house and the door immediately shut behind me. It was too dark and I couldn’t move. I felt a cold \(bodyPart1.text!) touch my \(bodyPart2.text!). I ran out the door and back into my home. I looked out the window into the house to see \(name2.text!) looking back at me."
+    }
+    
+    func story2() -> String? {
+        return "He awoke to the huge, \(noun1.text!) like creatures looming over his bed and screamed his \(bodyPart1.text!) out. They hastily left the room and he stayed up all night, \(verb1.text!) and wondering if it had been a dream. The next morning, there was a tap on the door. Gathering his courage, he opened it to see \(name1.text!) gently place a plate filled with fried breakfast on the floor, then retreat to a safe distance. Bewildered, he accepted the gift. The creatures chittered excitedly. This happened every day for weeks. At first he was \(adjective1.text!) they were fattening him up, but after a particularly greasy breakfast left him clutching his chest from heartburn, they were replaced with \(adjetive2.text!) fruit. As well as cooking, they poured hot steamy baths for him and even tucked him in when he went to bed. It was bizarre. One night, he awoke to gunshots and screaming. He raced downstairs to find a decapitated burglar being devoured by the creatures. He was \(adjetive3.text!), but disposed of the remains as best he could. He knew they had just been protecting him. One morning the creatures wouldn't let him leave his room. He lay down, confused but trusting as they ushered him back into bed. Whatever their motives, they weren't going to hurt him. Hours later a burning pain spread throughout his body. It felt like his \(bodyPart2.text!) was filled with razor wire. The creatures chittered as he spasmed and moaned. It was only when he felt a terrible squirming feeling beneath his \(bodyPart2.text!) that he realised the creatures hadn't been protecting him. They had been protecting their young."
     }
 }
